69375ea4cea59eff4822be1b7b1fb4b0ad65b65a
Ryan Mark <ma_xingmin@163.com>
2021-06-11 10:22:43 +0800

    feat(geecache): 增加三级key-value获取体系：geecache-远程获取-自定义回调函数获取

--- geecache.go
+++ geecache.go
@@ -1,5 +1,10 @@
 package geecache
 
+import (
+	"errors"
+	"sync"
+)
+
 type Getter interface {
 	Get(key string) ([]byte, error)
 }
@@ -11,6 +16,75 @@ func (gf GetterFunc) Get(key string) ([]byte, error) {
 	return gf(key)
 }
 
+type Group struct {
+	name      string
+	getter    Getter
+	mainCache cache
+}
 
+var (
+	mu sync.RWMutex
+	groups = make(map[string]*Group)
+)
 
+func NewGroup(name string, maxBytes int64, getter Getter) (group *Group) {
+
+	if getter == nil {
+		panic("getter is nil")
+	}
+
+	group = &Group{
+		name, getter, cache{cacheBytes: maxBytes},
+	}
+
+	mu.Lock()
+	groups[name] = group
+	mu.Unlock()
+
+	return
+}
+
+func GetGroup(name string) *Group {
+	mu.RLock()
+	defer mu.RUnlock()
+
+	return groups[name]
+}
+
+func (g *Group) Get(key string) (r ByteView, err error) {
+
+	if key == "" {
+		return ByteView{}, errors.New("key is required")
+	}
+
+	// why can it be called?
+	r, ok := g.mainCache.get(key)
+	if ok {
+		return r, nil
+	}
+
+	return g.load(key)
+}
+
+func (g *Group) load(key string) (ByteView, error) {
+	return g.GetFromLocal(key)
+}
+
+func (g *Group) GetFromLocal(key string) (bv ByteView, err error) {
+	bytes, err := g.getter.Get(key)
+	if err != nil {
+		return ByteView{}, err
+	}
+
+	// prevent changing cache value unintentionally
+	bv = ByteView{cloneBytes(bytes)}
+
+	g.populateCache(key, bv)
+
+	return bv, nil
+}
+
+func (g *Group) populateCache(key string, value ByteView) {
+	g.mainCache.add(key, value)
+}
 
--- geecache_test.go
+++ geecache_test.go
@@ -1,6 +1,8 @@
 package geecache
 
 import (
+	"fmt"
+	"log"
 	"reflect"
 	"testing"
 )
@@ -16,6 +18,56 @@ func TestGetterFunc_Get(t *testing.T) {
 
 	if !reflect.DeepEqual(got, expect) {
 		t.Fatalf("expect %v, got %v", expect, got)
+	}
+
+}
+
+var db = map[string]string{
+	"Tom":  "630",
+	"Jack": "589",
+	"Sam":  "567",
+}
+
+func TestGet(t *testing.T) {
+
+	loadCounts := make(map[string]int, len(db))
+
+	group := NewGroup("score", 1<<10, GetterFunc(func(key string) ([]byte, error) {
+
+		r, ok := db[key]
+		if ok {
+			log.Println("[slow db] search key", key)
+
+			_, ok = loadCounts[key]
+			if !ok {
+				loadCounts[key] = 0
+			}
+
+			loadCounts[key] += 1
+
+			return []byte(r), nil
+		}
+
+		return nil, fmt.Errorf("%s not exist", key)
+
+	}))
+
+	for k, v := range db {
+
+		vv, err := group.Get(k)
+		if err != nil || v != string(vv.b) {
+			t.Fatalf("error get key %s, expect %s, got %s", k, v, vv)
+		}
+
+		_, err = group.Get(k)
+		if err != nil || loadCounts[k] > 1 {
+			t.Fatalf("cache %s miss", k)
+		}
+	}
+
+	vv, err := group.Get("unknown")
+	if err == nil {
+		t.Fatalf("the value unknown should be empty, but %s got", vv)
 	}
 
 }
